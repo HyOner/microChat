104ea4d9f3c5bd2a482414e110cdfce1c4e2d696
HanYi <han_joe@outlook.com>
2019-06-04 03:41:32 +0800

    build 2

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(bodyParser.json());
-app.all('*', function (req, res, next) {
+// app.all('*', function (req, res, next) {
   // res.header("Access-Control-Allow-Origin", "*");
   // res.header("Access-Control-Allow-Headers", "X-Requested-With");
   // res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   // res.header("X-Powered-By", ' 3.2.1');
   // res.header("Content-Type", "application/json;charset=utf-8");
-  next();
-});
+  // next();
+// });
 
 app.get('/?chatroom', (req, res) => {
   res.sendFile(__dirname + '/index.html')
